54d6f3955eeb1d96442987a6d208355951db7e4a
Dhruv-Panwar042 <dhruvspanwar12@gmail.com>
2025-06-20 16:32:30 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+        function toggleMenu() {
+            document.getElementById('navLinks').classList.toggle('active');
+        }
+
+        function toggleMenu() {
+            document.getElementById('navLinks').classList.toggle('active');
+        }
